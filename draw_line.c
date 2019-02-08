23e2894e635a8d89db2b33a1aea6546fa4d9e091
Theo FERRIEUX <tferrieu@e1r12p23.42.fr>
2019-02-08 17:31:17 +0100

    gradient color

--- draw_line.c
+++ draw_line.c
@@ -6,24 +6,45 @@
 /*   By: magrab <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/18 20:30:03 by magrab            #+#    #+#             */
-/*   Updated: 2019/02/08 15:54:02 by magrab           ###   ########.fr       */
+/*   Updated: 2019/02/08 17:30:55 by tferrieu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-void	fill_pixel(t_fdf *fdf, int x, int y, int color)
+void		fill_pixel(t_fdf *fdf, int x, int y, int color)
 {
 	if (0 <= x && x < fdf->p_win.sx && 0 <= y && y < fdf->p_win.sy)
 		fdf->istr[x + y * fdf->p_win.sx] = color;
 }
 
-void	fill_line(t_fdf *fdf, t_pos pos0, t_pos pos1)
+static int	grad(t_pos orig, t_pos current, t_pos end)
+{
+	float	r;
+	int		color;
+	t_pos	bot;
+	t_pos	top;
+
+	top = orig.x > end.x ? orig : end;
+	bot = orig.x > end.x ? end : orig;
+	r = fabsf((float)(current.x - bot.x) / (float)(top.x - bot.x));
+	color = 0;
+	color += 0x10000 * (int)((r * (top.c / 0x10000)) +
+			((1 - r) * (bot.c / 0x10000)));
+	color += 0x100 * (int)((r * (top.c % 0x10000 / 0x100)) +
+			((1 - r) * (bot.c % 0x10000 / 0x100)));
+	color += (int)((r * (top.c % 0x100)) + ((1 - r) * (bot.c % 0x100)));
+	return (color);
+}
+
+void		fill_line(t_fdf *fdf, t_pos pos0, t_pos pos1)
 {
 	t_pos	d;
+	t_pos	orig;
 	int		err;
 	int		e2;
 
+	orig = pos0;
 	d.x = ft_abs(pos1.x - pos0.x);
 	d.y = ft_abs(pos1.y - pos0.y);
 	d.z = (pos0.x < pos1.x ? 1 : -1);
@@ -32,7 +53,7 @@ void	fill_line(t_fdf *fdf, t_pos pos0, t_pos pos1)
 	fill_pixel(fdf, pos0.x, pos0.y, pos0.c);
 	while (pos0.x != pos1.x && pos0.y != pos1.y)
 	{
-		fill_pixel(fdf, pos0.x, pos0.y, pos1.c);
+		fill_pixel(fdf, pos0.x, pos0.y, grad(orig, pos0, pos1);
 		e2 = err;
 		if (e2 > -d.x)
 		{
@@ -48,9 +69,9 @@ void	fill_line(t_fdf *fdf, t_pos pos0, t_pos pos1)
 }
 
 /*
-** d.z = sx
-** d.c = sy
-*/
+ ** d.z = sx
+ ** d.c = sy
+ */
 void	draw_line(t_fdf *fdf, t_pos pos0, t_pos pos1)
 {
 	t_pos	d;
