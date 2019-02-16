cbabb8f4f1055c18ec2b637d7bafebe2c81b0881
Theo FERRIEUX <tferrieu@e2r9p19.42.fr>
2019-02-16 16:16:26 +0100

    normified draw_line

--- draw_line.c
+++ draw_line.c
@@ -6,7 +6,7 @@
 /*   By: magrab <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/18 20:30:03 by magrab            #+#    #+#             */
-/*   Updated: 2019/02/08 17:33:17 by magrab           ###   ########.fr       */
+/*   Updated: 2019/02/16 16:14:44 by tferrieu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,24 +55,19 @@ void		fill_line(t_fdf *fdf, t_pos pos0, t_pos pos1)
 	{
 		fill_pixel(fdf, pos0.x, pos0.y, grad(orig, pos0, pos1));
 		e2 = err;
-		if (e2 > -d.x)
-		{
-			err -= d.y;
+		if (e2 > -d.x && ((err -= d.y) || 1))
 			pos0.x += d.z;
-		}
-		if (e2 < d.y)
-		{
-			err += d.x;
+		if (e2 < d.y && ((err += d.x) || 1))
 			pos0.y += d.c;
-		}
 	}
 }
 
 /*
- ** d.z = sx
- ** d.c = sy
- */
-void	draw_line(t_fdf *fdf, t_pos pos0, t_pos pos1)
+** d.z = sx
+** d.c = sy
+*/
+
+void		draw_line(t_fdf *fdf, t_pos pos0, t_pos pos1)
 {
 	t_pos	d;
 	int		err;
@@ -82,9 +77,9 @@ void	draw_line(t_fdf *fdf, t_pos pos0, t_pos pos1)
 	d.y = ft_abs(pos1.y - pos0.y);
 	d.z = (pos0.x < pos1.x ? 1 : -1);
 	d.c = (pos0.y < pos1.y ? 1 : -1);
-	err = (d.x > d.y ? d.x : -d.y);
+	err = (d.x > d.y ? d.x : -d.y) / 2;
 	mlx_pixel_put(fdf->mlx, fdf->win, pos0.x, pos0.y, pos0.c);
-	while (pos0.x != pos1.x && pos0.y != pos1.y)
+	while (pos0.x != pos1.x || pos0.y != pos1.y)
 	{
 		mlx_pixel_put(fdf->mlx, fdf->win, pos0.x, pos0.y, pos1.c);
 		e2 = err;
@@ -98,33 +93,5 @@ void	draw_line(t_fdf *fdf, t_pos pos0, t_pos pos1)
 			err += d.x;
 			pos0.y += d.c;
 		}
-	}
-}
-
-
-void	test_draw(t_fdf *fdf, int x, int y)
-{
-	static t_pos pos0 = {0, 0};
-	static t_pos pos1 = {0, 0};
-
-	if (pos0.x == 0)
-	{
-		pos0.x = x;
-		pos0.y = y;
-		pos0.c = 0xFFFFFF;
-	}
-	else if (pos1.x == 0)
-	{
-		pos1.x = x;
-		pos1.y = y;
-		pos1.c = 0xFFFFFF;
-	}
-	if (pos0.x != 0 && pos1.x != 0)
-	{
-		draw_line(fdf, pos0, pos1);
-		pos0.x = 0;
-		pos0.y = 0;
-		pos1.x = 0;
-		pos1.y = 0;
 	}
 }
