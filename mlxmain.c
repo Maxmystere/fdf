b9d2cb844c7f167dd8cded368c35a24731fbfcd0
Theo FERRIEUX <tferrieu@e2r9p19.42.fr>
2019-02-16 16:32:39 +0100

    normified mlxmain

--- mlxmain.c
+++ mlxmain.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fdfmain.c                                          :+:      :+:    :+:   */
+/*   mlxmain.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: magrab <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: tferrieu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/01/21 16:53:32 by magrab            #+#    #+#             */
-/*   Updated: 2019/02/14 14:15:46 by magrab           ###   ########.fr       */
+/*   Created: 2019/02/16 16:32:14 by tferrieu          #+#    #+#             */
+/*   Updated: 2019/02/16 16:32:17 by tferrieu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,22 @@
 
 void			show_menu(t_fdf *fdf)
 {
-	mlx_string_put(fdf->mlx, fdf->win, 10, 10, 0xFFFFFF, "/  Tuto !                              \\");
-	mlx_string_put(fdf->mlx, fdf->win, 10, 30, 0xFFFFFF, "|  Change Projection : 1, 2, 3         |");
-	mlx_string_put(fdf->mlx, fdf->win, 10, 50, 0xFFFFFF, "|  Press current Projection to rotate  |");
-	mlx_string_put(fdf->mlx, fdf->win, 10, 70, 0xFFFFFF, "|  Move : Arrow Keys                   |");
-	mlx_string_put(fdf->mlx, fdf->win, 10, 90, 0xFFFFFF, "|  Change Altitude : + / -             |");
-	mlx_string_put(fdf->mlx, fdf->win, 10, 110, 0xFFFFFF,"|  Zoom in/out : 8 / 2 (NUMPAD         |");
-	mlx_string_put(fdf->mlx, fdf->win, 10, 130, 0xFFFFFF,"|  Show this Menu : M                  |");
-	mlx_string_put(fdf->mlx, fdf->win, 10, 170, 0xFFFFFF,"|  Quit : Esc                          |");
+	mlx_string_put(fdf->mlx, fdf->win, 10, 10, 0xFFFFFF,
+								"/  Tuto !                              \\");
+	mlx_string_put(fdf->mlx, fdf->win, 10, 30, 0xFFFFFF,
+								"|  Change Projection : 1, 2, 3         |");
+	mlx_string_put(fdf->mlx, fdf->win, 10, 50, 0xFFFFFF,
+								"|  Press current Projection to rotate  |");
+	mlx_string_put(fdf->mlx, fdf->win, 10, 70, 0xFFFFFF,
+								"|  Move : Arrow Keys                   |");
+	mlx_string_put(fdf->mlx, fdf->win, 10, 90, 0xFFFFFF,
+								"|  Change Altitude : + / -             |");
+	mlx_string_put(fdf->mlx, fdf->win, 10, 110, 0xFFFFFF,
+								"|  Zoom in/out : 8 / 2 (NUMPAD         |");
+	mlx_string_put(fdf->mlx, fdf->win, 10, 130, 0xFFFFFF,
+								"|  Show this Menu : M                  |");
+	mlx_string_put(fdf->mlx, fdf->win, 10, 170, 0xFFFFFF,
+								"|  Quit : Esc                          |");
 }
 
 int				main(int ac, char **av)
